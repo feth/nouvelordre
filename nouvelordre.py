06165b0b9225d3c1a088e9f01da8a3fa963ab3cf
Feth Arezki <feth@tuttu.info>
2011-01-31 14:14:46 +0100

    apply nouvelordre.py to itself

--- nouvelordre.py
+++ nouvelordre.py
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 
-from _ast import ImportFrom, Import
+from _ast import Import, ImportFrom
 from ast import parse
 from enthought.traits.api import Dict, Either, HasTraits, Int, List, Str, Tuple
 
