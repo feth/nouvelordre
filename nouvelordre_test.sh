83044fcf3d82f598f5bcca4670cbb201bbdc9dbf
Feth Arezki <feth@tuttu.info>
2011-01-31 22:24:57 +0100

    more error handling, used in tests

--- nouvelordre_test.sh
+++ nouvelordre_test.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 for file in $(locate "*.py")
 	do echo "testing $file"
-	reorder --infile $file --outfile /dev/null --dump
-	if [ $? != 0 ] ; then
-		echo "Please send the bug report (nouvelordre_bug_report) to feth@tuttu.info. Many thanks."
-		exit 1
+
+	if [ ! -f $file ] ; then
+		echo "file $file not found or not a file."
+		continue
 	fi
+
+	reorder --infile $file --outfile /dev/null --dump
+	case $? in
+		129)
+			echo "Seems the file is not compiling, ignoring error."
+			;;
+		130)
+			echo "NotImplementedError ! Ignoring anyway"
+			;;
+		0)
+			echo "...ok"
+			;;
+		*)
+			echo "Please send the bug report (nouvelordre_bug_report) to feth@tuttu.info. Many thanks."
+			exit 1
+			;;
+	esac
 done
